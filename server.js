00a25d4f9e2a5e865e03760c5bf5fd5c6301a09d
Tachi <zaveruha007@gmail.com>
2015-06-25 12:46:28 +0300

    Configs

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+//Require Modules
 var http = require('http');
 var url = require('url');
 var path = require('path');
 var fs = require('fs');
+
+//Creating Array of available mimeTypes (https://en.wikipedia.org/wiki/Internet_media_type)
+var mimeTypes = {
+	"html" : "text/html",
+	"jpeg" : "image/jpeg",
+	"jpg" : "image/jpeg",
+	"png" : "image/png",
+	"js" : "text/javascript",
+	"css" : "text/css"
+};
