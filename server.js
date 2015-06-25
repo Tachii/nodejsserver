8a02cbe77012fa0133cf5554b5dc76626bb7fdf2
Tachi <zaveruha007@gmail.com>
2015-06-25 13:33:31 +0300

    Config

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 //Require Modules
+var express = require('express');
+var app = express();
 var http = require('http');
 var url = require('url');
 var path = require('path');
 var fs = require('fs');
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+	response.send('Hello World');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Node app is running on port: ', app.get('port'));
+});
 
 //Creating Array of available mimeTypes (https://en.wikipedia.org/wiki/Internet_media_type)
 var mimeTypes = {
